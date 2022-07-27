d972e12f864946f52fd801c895208f81251d10ff
Jeonghyeon Park <dotsnangles@gmail.com>
2022-07-27 15:06:50 +0900

    ADD response_table

--- modules.py
+++ modules.py
@@ -1,4 +1,4 @@
-import pickle, os, gdown
+import pickle, os, gdown, shutil
 import pandas as pd
 import numpy as np
 import torch
@@ -8,10 +8,18 @@ from transformers import BertPreTrainedModel, BertConfig, BertModel, BertTokeniz
 from encoder.encoder import PolyEncoder
 from encoder.transform import SelectionJoinTransform, SelectionSequentialTransform
 
+if os.path.exists('./data'):
+    shutil.rmtree('./data')
+
+if os.path.exists('./model'):
+    shutil.rmtree('./model')
+
 gdown.download_folder(id='1Ipr-aNF5ELMY0HTXAmeV26LlgktKUfmG', quiet=True, use_cookies=False)
 gdown.download_folder(id='1RH7laK4WlucCw68ZeExFvyg7vs-kB_x3', quiet=True, use_cookies=False)
 os.rename('./감성대화챗봇데이터/', './data')
 os.rename('./chatbot_output/', './model')
+
+response_table = pd.read_csv('./data/response_data.csv')
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # print(device)
